198553123f0198ddb2eb10eb53b40f7c768da368
Shawon <chiefmahedi@gmail.com>
2024-03-24 23:36:45 +0600

    Add solution of Binary Search from LeetCode using TypeScript

---
+++ Binary Search/Binary Search (LeetCode)/TypeScript/Solution1/solution1.ts
@@ -0,0 +1,18 @@
+function search(nums: number[], target: number): number {
+    let left : number = 0;
+    let right: number = nums.length - 1;
+    while(left<=right)
+    {
+        const middle = (left + right) >> 1;
+        if(nums[middle] > target)
+        {
+            right = middle - 1;
+        }
+        else if(nums[middle] < target)
+        {
+            left = middle + 1;
+        }
+        else return middle;
+    }
+    return -1;
+};
