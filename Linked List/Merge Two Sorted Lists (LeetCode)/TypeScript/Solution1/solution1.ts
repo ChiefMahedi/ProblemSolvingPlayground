bb6b34d9dc56c40f0609cc9f9999088f44bc6dc3
Shawon <chiefmahedi@gmail.com>
2024-05-18 23:17:44 +0600

    Add solution of Merge Two Sorted Lists from LeetCode using TypeScript

--- Linked List/Merge Two Sorted Lists (LeetCode)/TypeScript/Solution1/solution1.ts
+++ Linked List/Merge Two Sorted Lists (LeetCode)/TypeScript/Solution1/solution1.ts
@@ -11,5 +11,23 @@
  */
 
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
-    
+    let resultList: ListNode = new ListNode(0);
+    let node: ListNode = resultList;
+
+    while (list1 !== null && list2 !== null) {
+        if (list1.val < list2.val) {
+            node.next = list1;
+            list1 = list1.next;
+        } else {
+            node.next = list2;
+            list2 = list2.next;
+        }
+
+        node = node.next;
+    }
+
+    if (list1 !== null) node.next = list1;
+    if (list2 !== null) node.next = list2;
+
+    return resultList.next;
 };
